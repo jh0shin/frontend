bc3e37a7982d8787ab123c885d1557d47e3a2df1
yjsong2154 <yjsong2153@gmail.com>
2020-03-24 22:54:01 +0900

    mac issue test'

--- src/components/HomeSelectField.js
+++ src/components/HomeSelectField.js
@@ -9,8 +9,8 @@ class HomeSelectField extends React.Component{
   render(){
     return (
         <div className="field">
-          <h1 id="requestTitle">REQUEST</h1>
-          <h2>자문 요청</h2>
+          <h1 id = "requestTextOne">REQUEST</h1>
+          <h2 id = "requestTextTwo">자문 요청</h2>
           {this.props.isParent ? <HomeOfflineInput/> : <HomeLectureInput/>}
           {this.props.isParent ? <Link id="companyButton" to="/company">기업 자문 요청 페이지로 가기</Link> : ""}
         </div>
