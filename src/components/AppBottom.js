4b72d12df34a012d8cbd19c691c3a7298d2b686d
yjsong2154 <yjsong2153@gmail.com>
2020-05-17 17:13:46 +0900

    bottom num3

--- src/components/AppBottom.js
+++ src/components/AppBottom.js
@@ -4,8 +4,9 @@ import './AppBottom.css';
 function AppBottom() {
   return (
     <div className="bottom">
-      <p>서울시 성북구 안암로 145 고려대학교 KU개척마을 S304호</p>
-      <p>대표자 : 이민수 (등록번호 : 240-86-01519)</p>
+      {/* <p>서울시 성북구 안암로 145 고려대학교 KU개척마을 S304호</p> */}
+      <p>경기도 성남시 분당구 내정로 152, 127동 602호</p>
+      <p>대표자 : 이민수 (사업자 등록번호 : 240-86-01519)</p>
       <p>phone : 010-2645-3704</p>
       <p>email : contact@hakwongo.com</p>
 			<p>2020 ㈜ 학원고 All rights reserved</p> <br></br>
